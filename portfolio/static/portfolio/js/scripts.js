3ea7ca322836a3fe35f4d71f857a35e4b31d491a
Jerry Bao <thejerrybao@gmail.com>
2015-02-02 17:43:34 -0800

    changed header from top animation to fade

--- portfolio/static/portfolio/js/scripts.js
+++ portfolio/static/portfolio/js/scripts.js
@@ -7,45 +7,38 @@ var scrollVal;
 var scrolled = false;
 
 $(document).ready(function() {
+    $(".header-small").hide();
     $(window).scroll(function() {
         scrollVal = $(this).scrollTop();
         if (scrollVal > 124) {
-            $(".header").css({
-                'position': 'fixed',
-                'top': '-125px'
-            });
-            $(".main-page-nav").css({
-                'position': 'fixed',
-                'top': '105px'
-            });
-            $("body").css({
-                'margin-top': '210px'
-            });
             if (!scrolled) {
                 scrolled = true;
-                $(".header-big").css({
+                $(".header").css({
                     'position': 'fixed',
                     'top': '-125px'
-                })
-                $(".header-small").animate({ "top": "0px"}, "fast");
-                $(".header-big").animate({ "top": "-210px"}, "fast");
+                });
+                $(".main-page-nav").css({
+                    'position': 'fixed',
+                    'top': '105px'
+                });
+                $("body").css({
+                    'margin-top': '210px'
+                });
+                $(".header-small, .header-big").fadeToggle();
             }
         } else {
-            $(".header").css({
-                'position': 'static'
-            });
-            $(".main-page-nav").css({
-                'position': 'static'
-            });
-            $("body").css({
-                'margin-top': '0'
-            });
             if (scrolled) {
                 scrolled = false;
-                $(".header-small").animate({ "top": "-100px"}, "fast");
-                $(".header-big").css({
+                $(".header").css({
                     'position': 'static'
                 });
+                $(".main-page-nav").css({
+                    'position': 'static'
+                });
+                $("body").css({
+                    'margin-top': '0'
+                });
+                $(".header-small, .header-big").fadeToggle();
             }
         }
     });
